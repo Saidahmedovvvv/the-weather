e66b9f2b9571cc933ad911a3b64b0c96c3d3bcd8
Said Ahmedov <89637007721@mail.ru>
2022-01-10 20:06:40 +0300

    change js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=a77b9f2616cdc1cc9b61a0aa0ba72e5f')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=a77b9f2616cdc1cc9b61a0aa0ba72e5f')
   .then(function (resp) {
     return resp.json()
   })
@@ -6,7 +6,7 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appi
     document.querySelector('.city-name').textContent = data.name;
     document.querySelector('.deg').innerHTML = Math.round(data.main.temp) + '&deg;';
     document.querySelector('.weather').textContent = data.weather[0]['description'];
-    document.querySelector('.icons').innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png" alt="" />`;
+    document.querySelector('.icons').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png" alt="" />`;
   })
   .catch(function () {
 
