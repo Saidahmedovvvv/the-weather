b3af970281dd933c74aaf7886f10a4f2a5b2de9e
Said Ahmedov <89637007721@mail.ru>
2022-01-10 19:50:51 +0300

    the weather

---
+++ js/main.js
@@ -0,0 +1,17 @@
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=a77b9f2616cdc1cc9b61a0aa0ba72e5f')
+  .then(function (resp) {
+    return resp.json()
+  })
+  .then(function (data) {
+    document.querySelector('.city-name').textContent = data.name;
+    document.querySelector('.deg').innerHTML = Math.round(data.main.temp) + '&deg;';
+    document.querySelector('.weather').textContent = data.weather[0]['description'];
+    document.querySelector('.icons').innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png" alt="" />`;
+  })
+  .catch(function () {
+
+  });
+
+// http://openweathermap.org/img.wn.04d@2x.png
+
+// a77b9f2616cdc1cc9b61a0aa0ba72e5f
